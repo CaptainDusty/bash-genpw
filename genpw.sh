538eb479af0e67304372c72bee9fb712407fb671
CaptainDusty <dustyn@remexchange.net>
2016-09-05 07:32:44 +1000

    Added []<> as additional characters

--- genpw.sh
+++ genpw.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-d=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()_+' | head -c 3`
-a=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()_+' | head -c 3`
-b=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()_+' | head -c 3`
-c=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()_+' | head -c 3`
+d=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | head -c 3`
+a=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | head -c 3`
+b=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | head -c 3`
+c=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | head -c 3`
 
 echo "$a-$b-$c-$d"
