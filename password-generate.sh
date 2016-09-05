40cafa2a43ac1e37ecef0088568744d00857025b
Dustyn <dustyn@remexchange.net>
2016-09-05 11:41:11 +1000

    Added LC_CTYPE=C which resolves an issue with OSx illegal byte sequence

--- password-generate.sh
+++ password-generate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-d=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
-a=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
-b=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
-c=`cat /dev/urandom | tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
+d=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
+a=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
+b=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
+c=`cat /dev/urandom | LC_CTYPE=C tr -dc '0-9a-zA-Z!@#$%^&*()[]<>_+' | tr 'oO0iI1lL7sS5' 'aA8bB3cC4dD9' | head -c 3`
 
 echo "Password: $a-$b-$c-$d"
